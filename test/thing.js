9836d7f736a58bde09a0b8f871e0a7bc10b4271c
VinkentLi <vinkentclicker27@gmail.com>
2025-03-15 18:58:11 -0400

    reduced size of data

--- test/thing.js
+++ test/thing.js
@@ -10,10 +10,10 @@ fs.createReadStream("codejoint.png")
     })
   )
   .on("parsed", function () {
-    for (var y = 0; y < this.height; y++) {
+    for (var y = 0; y < this.height; y += 2.5) {
         let row = [];
-        for (var x = 0; x < this.width; x++) {
-          var idx = (this.width * y + x) << 2;
+        for (var x = 0; x < this.width; x += 2.5) {
+          var idx = (this.width * Math.floor(y) + Math.floor(x)) << 2;
           row.push(this.data[idx+3] > 128 ? 1 : 0);
         }
         result += JSON.stringify(row) + ",\n";
